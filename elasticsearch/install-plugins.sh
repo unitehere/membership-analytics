485115dd390738585bbc1ca2464a22228f5c2900
Han Xiao <hxiao@anthemengineering.com>
2017-08-30 14:26:45 -0400

    Add Elasticsearch Plugin Install Script
    Add script to install necessary Elasticsearch plugins.

---
+++ elasticsearch/install-plugins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root." 
+  exit 1
+fi
+
+if [[ -z "${ES_HOME}" ]]; then
+  echo "Where is elasticsearch-plugin installed (e.g. /usr/share/elasticsearch/bin)?"
+  read ES_PLUGIN_DIR
+else
+  ES_PLUGIN_DIR="${ES_HOME}/bin"
+fi
+
+cd $ES_PLUGIN_DIR
+./elasticsearch-plugin install analysis-phonetic
+
+cd -
