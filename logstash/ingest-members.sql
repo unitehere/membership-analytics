290fada8118141615e039b626a0a4009308d1a33
Justin Patterson <jpatterson@anthemengineering.com>
2017-09-05 12:14:15 -0400

    SQL Query now will leave out any fields with NULL values from the JSON results

--- logstash/ingest-members.sql
+++ logstash/ingest-members.sql
@@ -4,21 +4,21 @@ SELECT n.id, STUFF(
   (
     SELECT ', {' +
     '"imis_id": "' + n1.ID + '",' +
-    '"org_code": "' + COALESCE(n1.ORG_CODE,'') + '",' +
-    '"member_type": "' + COALESCE(n1.MEMBER_TYPE,'') + '",' +
-    '"category": "' + COALESCE(n1.CATEGORY,'') + '",' +
-    '"status": "' + COALESCE(n1.[STATUS],'') + '",' +
-    '"title": "' + COALESCE(n1.TITLE,'') + '",' +
-    '"company": "' + STRING_ESCAPE(COALESCE(n1.COMPANY,''), 'json') + '",' +
-    '"prefix": "' + COALESCE(n1.PREFIX,'') + '",' +
-    '"first_name": "' + COALESCE(n1.FIRST_NAME,'') + '",' +
-    '"middle_name": "' + COALESCE(MIDDLE_NAME,'') + '",' +
-    '"last_name": "' + COALESCE(n1.LAST_NAME,'') + '",' +
-    '"suffix": "' + COALESCE(n1.SUFFIX,'') + '",' +
-    '"designation": "' + COALESCE(n1.DESIGNATION,'') + '",' +
-    '"chapter": "' + COALESCE(n1.CHAPTER,'') + '",' +
-    '"functional_title": "' + COALESCE(n1.FUNCTIONAL_TITLE,'') + '",' +
-    '"member_status": "' + COALESCE(n1.MEMBER_STATUS,'') + '"' +
+    CASE WHEN n1.ORG_CODE IS NULL THEN '' ELSE '"org_code": "' + COALESCE(n1.ORG_CODE,'') + '",' END +
+    CASE WHEN n1.MEMBER_TYPE IS NULL THEN '' ELSE '"member_type": "' + COALESCE(n1.MEMBER_TYPE,'') + '",' END +
+    CASE WHEN n1.CATEGORY IS NULL THEN '' ELSE '"category": "' + COALESCE(n1.CATEGORY,'') + '",' END +
+    CASE WHEN n1.[STATUS] IS NULL THEN '' ELSE '"status": "' + COALESCE(n1.[STATUS],'') + '",' END +
+    CASE WHEN n1.TITLE IS NULL THEN '' ELSE '"title": "' + COALESCE(n1.TITLE,'') + '",' END +
+    CASE WHEN n1.COMPANY IS NULL THEN '' ELSE '"company": "' + STRING_ESCAPE(COALESCE(n1.COMPANY,''), 'json') + '",' END +
+    CASE WHEN n1.PREFIX IS NULL THEN '' ELSE '"prefix": "' + COALESCE(n1.PREFIX,'') + '",' END +
+    CASE WHEN n1.FIRST_NAME IS NULL THEN '' ELSE '"first_name": "' + COALESCE(n1.FIRST_NAME,'') + '",' END +
+    CASE WHEN n1.MIDDLE_NAME IS NULL THEN '' ELSE '"middle_name": "' + COALESCE(MIDDLE_NAME,'') + '",' END +
+    CASE WHEN n1.LAST_NAME IS NULL THEN '' ELSE '"last_name": "' + COALESCE(n1.LAST_NAME,'') + '",' END +
+    CASE WHEN n1.SUFFIX IS NULL THEN '' ELSE '"suffix": "' + COALESCE(n1.SUFFIX,'') + '",' END +
+    CASE WHEN n1.DESIGNATION IS NULL THEN '' ELSE '"designation": "' + COALESCE(n1.DESIGNATION,'') + '",' END +
+    CASE WHEN n1.CHAPTER IS NULL THEN '' ELSE '"chapter": "' + COALESCE(n1.CHAPTER,'') + '",' END +
+    CASE WHEN n1.FUNCTIONAL_TITLE IS NULL THEN '' ELSE '"functional_title": "' + COALESCE(n1.FUNCTIONAL_TITLE,'') + '",' END +
+    CASE WHEN n1.MEMBER_STATUS IS NULL THEN '' ELSE '"member_status": "' + COALESCE(n1.MEMBER_STATUS,'') + '"' END +
     '}'
   FROM dbo.Name n1
   WHERE n1.id = n.id
@@ -30,18 +30,18 @@ SELECT n.id, STUFF(
     (
       SELECT ', {' +
       '"imis_seqn": "' + COALESCE(convert(varchar(50),a1.SEQN),'') + '",' +
-      '"team": "' + COALESCE(a1.TEAM,'') + '",' +
-      '"preferred": ' + COALESCE(convert(varchar(1),a1.PREFERRED),'') + ',' +
-      '"address_type": "' + COALESCE(a1.ADDRESS_TYPE,'') + '",' +
-      '"address_1": "' + COALESCE(a1.ADDRESS_1,'') + '",' +
-      '"address_2": "' + COALESCE(a1.ADDRESS_2,'') + '",' +
-      '"address_3": "' + COALESCE(a1.ADDRESS_3,'') + '",' +
-      '"city": "' + COALESCE(a1.CITY,'') + '",' +
-      '"state_province": "' + COALESCE(a1.STATE_PROVINCE,'') + '",' +
-      '"zip": "' + COALESCE(a1.ZIP,'') + '",' + 
-      '"country": "' + COALESCE(a1.COUNTRY,'') + '",' +
-      '"note": "' + COALESCE(a1.NOTE,'') + '",' +
-      '"validated": ' + COALESCE(convert(varchar(1), a1.VALIDATED),'') + 
+      CASE WHEN a1.TEAM IS NULL THEN '' ELSE '"team": "' + COALESCE(a1.TEAM,'') + '",' END +
+      CASE WHEN a1.preferred IS NULL THEN '' ELSE '"preferred": ' + COALESCE(convert(varchar(1),a1.PREFERRED),'') + ',' END +
+      CASE WHEN a1.ADDRESS_TYPE IS NULL THEN '' ELSE '"address_type": "' + COALESCE(a1.ADDRESS_TYPE,'') + '",' END +
+      CASE WHEN a1.ADDRESS_1 IS NULL THEN '' ELSE '"address_1": "' + COALESCE(a1.ADDRESS_1,'') + '",' END +
+      CASE WHEN a1.ADDRESS_2 IS NULL THEN '' ELSE '"address_2": "' + COALESCE(a1.ADDRESS_2,'') + '",' END +
+      CASE WHEN a1.ADDRESS_3 IS NULL THEN '' ELSE '"address_3": "' + COALESCE(a1.ADDRESS_3,'') + '",' END +
+      CASE WHEN a1.CITY IS NULL THEN '' ELSE '"city": "' + COALESCE(a1.CITY,'') + '",' END +
+      CASE WHEN a1.STATE_PROVINCE IS NULL THEN '' ELSE '"state_province": "' + COALESCE(a1.STATE_PROVINCE,'') + '",' END +
+      CASE WHEN a1.ZIP IS NULL THEN '' ELSE '"zip": "' + COALESCE(a1.ZIP,'') + '",' END + 
+      CASE WHEN a1.COUNTRY IS NULL THEN '' ELSE '"country": "' + COALESCE(a1.COUNTRY,'') + '",' END +
+      CASE WHEN a1.NOTE IS NULL THEN '' ELSE '"note": "' + COALESCE(a1.NOTE,'') + '",' END +
+      CASE WHEN a1.VALIDATED IS NULL THEN '' ELSE '"validated": ' + COALESCE(convert(varchar(1), a1.VALIDATED),'') END + 
       '}'
   FROM dbo.UH_ADDRESS a1
   WHERE a1.id = n.id
@@ -53,13 +53,13 @@ SELECT n.id, STUFF(
       (
         SELECT ',  {' +
         '"imis_seqn": "' + COALESCE(convert(varchar(50),e1.SEQN),'') + '",' +
-        '"team": "' + COALESCE(e1.TEAM,'') + '",' +
-        '"preferred": ' + COALESCE(convert(varchar(50),e1.PREFERRED),'') + ',' +
-        '"email_type": "' + COALESCE(e1.EMAIL_TYPE,'') + '",' +
-        '"email": "' + COALESCE(e1.EMAIL,'') + '",' + 
-        '"note": "' + COALESCE(e1.NOTE,'') + '",' +
-        '"bad": "' + COALESCE(convert(varchar(50), e1.BAD, 121),'') + '",' +
-        '"permission_to_email": "' + COALESCE(e1.PERMISSION_TO_EMAIL,'') + '"' +
+        CASE WHEN e1.TEAM IS NULL THEN '' ELSE '"team": "' + COALESCE(e1.TEAM,'') + '",' END +
+        CASE WHEN e1.PREFERRED IS NULL THEN '' ELSE '"preferred": ' + COALESCE(convert(varchar(50),e1.PREFERRED),'') + ',' END +
+        CASE WHEN e1.EMAIL_TYPE IS NULL THEN '' ELSE '"email_type": "' + COALESCE(e1.EMAIL_TYPE,'') + '",' END +
+        CASE WHEN e1.EMAIL IS NULL THEN '' ELSE '"email": "' + COALESCE(e1.EMAIL,'') + '",' END + 
+        CASE WHEN e1.NOTE IS NULL THEN '' ELSE '"note": "' + COALESCE(e1.NOTE,'') + '",' END +
+        CASE WHEN e1.BAD IS NULL THEN '' ELSE '"bad": "' + COALESCE(convert(varchar(50), e1.BAD, 121),'') + '",' END +
+        CASE WHEN e1.PERMISSION_TO_EMAIL IS NULL THEN '' ELSE '"permission_to_email": "' + COALESCE(e1.PERMISSION_TO_EMAIL,'') + '"' END +
         '}'
   FROM dbo.UH_EMAIL e1
   WHERE e1.id = n.id
@@ -71,16 +71,16 @@ SELECT n.id, STUFF(
         (
           SELECT ', {' +
           '"imis_seqn": "' + COALESCE(convert(varchar(50),p1.SEQN),'') + '",' +
-          '"team": "' + COALESCE(p1.TEAM,'') + '",' +
-          '"preferred": ' + COALESCE(convert(varchar(50),p1.PREFERRED),'') + ',' +
-          '"phone_type": "' + COALESCE(p1.PHONE_TYPE,'') + '",' +
-          '"phone": "' + COALESCE(p1.PHONE,'') + '",' + 
-          '"optin_text": "' + COALESCE(p1.OPTIN_TEXT,'') + '",' +
-          '"contact_time": "' + COALESCE(p1.CONTACT_TIME,'') + '",' +
-          '"notes": "' + COALESCE(p1.NOTES,'') + '",' +
-           COALESCE('"permiss_to_text_date": "' +convert(varchar(50), p1.PERMISS_TO_TEXT_DATE, 121) + '",','') +
-          '"permiss_to_text_source": "' + COALESCE(p1.PERMISS_TO_TEXT_SOURCE,'') + '",' +
-          '"extension": "' + COALESCE(p1.EXTENSION,'') + '"' +
+          CASE WHEN p1.TEAM IS NULL THEN '' ELSE '"team": "' + COALESCE(p1.TEAM,'') + '",' END +
+          CASE WHEN p1.PREFERRED IS NULL THEN '' ELSE '"preferred": ' + COALESCE(convert(varchar(50),p1.PREFERRED),'') + ',' END +
+          CASE WHEN p1.PHONE_TYPE IS NULL THEN '' ELSE '"phone_type": "' + COALESCE(p1.PHONE_TYPE,'') + '",' END +
+          CASE WHEN p1.PHONE IS NULL THEN '' ELSE '"phone": "' + COALESCE(p1.PHONE,'') + '",' END + 
+          CASE WHEN p1.OPTIN_TEXT IS NULL THEN '' ELSE '"optin_text": "' + COALESCE(p1.OPTIN_TEXT,'') + '",' END +
+          CASE WHEN p1.CONTACT_TIME IS NULL THEN '' ELSE '"contact_time": "' + COALESCE(p1.CONTACT_TIME,'') + '",' END +
+          CASE WHEN p1.NOTES IS NULL THEN '' ELSE '"notes": "' + COALESCE(p1.NOTES,'') + '",' END +
+          CASE WHEN p1.PERMISS_TO_TEXT_DATE IS NULL THEN '' ELSE COALESCE('"permiss_to_text_date": "' +convert(varchar(50), p1.PERMISS_TO_TEXT_DATE, 121) + '",','') END +
+          CASE WHEN p1.PERMISS_TO_TEXT_SOURCE IS NULL THEN '' ELSE '"permiss_to_text_source": "' + COALESCE(p1.PERMISS_TO_TEXT_SOURCE,'') + '",' END +
+          CASE WHEN p1.EXTENSION IS NULL THEN '' ELSE '"extension": "' + COALESCE(p1.EXTENSION,'') + '"' END +
           '}'
   FROM dbo.UH_PHONE p1
   WHERE p1.id = n.id
@@ -92,17 +92,17 @@ SELECT n.id, STUFF(
           (
             SELECT ', {' +
             '"imis_seqn": "' + COALESCE(convert(varchar(50),q1.SEQN),'') + '",' +
-            '"employer_name": "' + STRING_ESCAPE(COALESCE(q1.EMPLOYER_NAME,''), 'json') + '",' +
-            '"employer_id": "' + COALESCE(q1.EMPLOYER_ID,'') + '",' +
-            '"employee_id": "' + COALESCE(q1.EMPLOYEE_ID,'') + '",' +
-            '"primary_employer": ' + COALESCE(CONVERT(varchar(1), q1.PRIMARY_EMPLOYER),'') + ',' + 
-            COALESCE('"effective_date": "' + convert(varchar(50), q1.EFFECTIVE_DATE, 121) + '",','') +
-            COALESCE('"thru_date": "' + convert(varchar(50), q1.THRU_DATE, 121) + '",','') +
-            '"classification": "' + COALESCE(q1.CLASSIFICATION,'') + '",' +
-            '"location": "' + COALESCE(q1.[LOCATION],'') + '",' +
-            '"dept": "' + COALESCE(q1.DEPT,'') + '",' +
-            '"shift": "' + COALESCE(q1.[SHIFT],'') + '",' +
-            '"work_hours": "' + COALESCE(q1.WORK_HOURS,'') + '"' + 
+            CASE WHEN q1.EMPLOYER_NAME IS NULL THEN '' ELSE '"employer_name": "' + STRING_ESCAPE(COALESCE(q1.EMPLOYER_NAME,''), 'json') + '",' END +
+            CASE WHEN q1.EMPLOYER_ID IS NULL THEN '' ELSE '"employer_id": "' + COALESCE(q1.EMPLOYER_ID,'') + '",' END +
+            CASE WHEN q1.EMPLOYEE_ID IS NULL THEN '' ELSE '"employee_id": "' + COALESCE(q1.EMPLOYEE_ID,'') + '",' END +
+            CASE WHEN q1.PRIMARY_EMPLOYER IS NULL THEN '' ELSE '"primary_employer": ' + COALESCE(CONVERT(varchar(1), q1.PRIMARY_EMPLOYER),'') + ',' END + 
+            CASE WHEN q1.EFFECTIVE_DATE IS NULL THEN '' ELSE COALESCE('"effective_date": "' + convert(varchar(50), q1.EFFECTIVE_DATE, 121) + '",','') END +
+            CASE WHEN q1.THRU_DATE IS NULL THEN '' ELSE COALESCE('"thru_date": "' + convert(varchar(50), q1.THRU_DATE, 121) + '",','') END +
+            CASE WHEN q1.CLASSIFICATION IS NULL THEN '' ELSE '"classification": "' + COALESCE(q1.CLASSIFICATION,'') + '",' END +
+            CASE WHEN q1.[LOCATION] IS NULL THEN '' ELSE '"location": "' + COALESCE(q1.[LOCATION],'') + '",' END +
+            CASE WHEN q1.DEPT IS NULL THEN '' ELSE '"dept": "' + COALESCE(q1.DEPT,'') + '",' END +
+            CASE WHEN q1.[SHIFT] IS NULL THEN '' ELSE '"shift": "' + COALESCE(q1.[SHIFT],'') + '",' END +
+            CASE WHEN q1.WORK_HOURS IS NULL THEN '' ELSE '"work_hours": "' + COALESCE(q1.WORK_HOURS,'') + '"' END + 
             '}'
   FROM dbo.UH_EMPLOYER q1
   WHERE q1.id = n.id
@@ -113,21 +113,21 @@ SELECT n.id, STUFF(
   STUFF(
              (
             SELECT ', {' +
-            '"ssn": "' + COALESCE(d1.SSN,'') + '",' +
-            '"ssn_src": "' + COALESCE(d1.SSN_SRC,'') + '",' +
-            '"ethnicity": "' + COALESCE(d1.ETHNICITY,'') + '",' +
-            '"ethnicity_src": "' + COALESCE(d1.ETHNICITY_SRC,'') + '",' +
-            '"country_origin": "' + COALESCE(d1.COUNTRY_ORIGIN,'') + '",' + 
-            '"country_origin_src": "' + COALESCE(d1.COUNTRY_ORIGIN_SRC,'') + '",' +
-            '"primary_language": "' + COALESCE(d1.PRIMARY_LANGUAGE,'') + '",' +
-            '"primary_language_src": "' + COALESCE(d1.PRIMARY_LANGUAGE_SRC,'') + '",' +
-            '"other_language": "' + COALESCE(d1.OTHER_LANGUAGE,'') + '",' +
-            '"other_language_src": "' + COALESCE(d1.OTHER_LANGUAGE,'') + '",' +
-            '"city_origin": "' + COALESCE(d1.CITY_ORIGIN,'') + '",' +
-            '"city_origin_src": "' + COALESCE(d1.CITY_ORIGIN_SRC,'') + '",' + 
-            '"gender": "' + COALESCE(d1.GENDER,'') + '",' +
-            '"gender_src": "' + COALESCE(d1.GENDER_SRC,'') + '",' +
-            '"other_gender": "' + COALESCE(d1.OTHER_GENDER,'') + '"' + 
+            CASE WHEN d1.SSN IS NULL THEN '' ELSE '"ssn": "' + COALESCE(d1.SSN,'') + '",' END +
+            CASE WHEN d1.SSN_SRC IS NULL THEN '' ELSE '"ssn_src": "' + COALESCE(d1.SSN_SRC,'') + '",' END +
+            CASE WHEN d1.ETHNICITY IS NULL THEN '' ELSE '"ethnicity": "' + COALESCE(d1.ETHNICITY,'') + '",' END +
+            CASE WHEN d1.ETHNICITY_SRC IS NULL THEN '' ELSE '"ethnicity_src": "' + COALESCE(d1.ETHNICITY_SRC,'') + '",' END +
+            CASE WHEN d1.COUNTRY_ORIGIN IS NULL THEN '' ELSE '"country_origin": "' + COALESCE(d1.COUNTRY_ORIGIN,'') + '",' END + 
+            CASE WHEN d1.COUNTRY_ORIGIN_SRC IS NULL THEN '' ELSE '"country_origin_src": "' + COALESCE(d1.COUNTRY_ORIGIN_SRC,'') + '",' END +
+            CASE WHEN d1.PRIMARY_LANGUAGE IS NULL THEN '' ELSE '"primary_language": "' + COALESCE(d1.PRIMARY_LANGUAGE,'') + '",' END +
+            CASE WHEN d1.PRIMARY_LANGUAGE_SRC IS NULL THEN '' ELSE '"primary_language_src": "' + COALESCE(d1.PRIMARY_LANGUAGE_SRC,'') + '",' END +
+            CASE WHEN d1.OTHER_LANGUAGE IS NULL THEN '' ELSE '"other_language": "' + COALESCE(d1.OTHER_LANGUAGE,'') + '",' END +
+            CASE WHEN d1.OTHER_LANGUAGE_SRC IS NULL THEN '' ELSE '"other_language_src": "' + COALESCE(d1.OTHER_LANGUAGE_SRC,'') + '",' END +
+            CASE WHEN d1.CITY_ORIGIN IS NULL THEN '' ELSE '"city_origin": "' + COALESCE(d1.CITY_ORIGIN,'') + '",' END +
+            CASE WHEN d1.CITY_ORIGIN_SRC IS NULL THEN '' ELSE '"city_origin_src": "' + COALESCE(d1.CITY_ORIGIN_SRC,'') + '",' END + 
+            CASE WHEN d1.GENDER IS NULL THEN '' ELSE '"gender": "' + COALESCE(d1.GENDER,'') + '",' END +
+            CASE WHEN d1.GENDER_SRC IS NULL THEN '' ELSE '"gender_src": "' + COALESCE(d1.GENDER_SRC,'') + '",' END +
+            CASE WHEN d1.OTHER_GENDER IS NULL THEN '' ELSE '"other_gender": "' + COALESCE(d1.OTHER_GENDER,'') + '"' END + 
             '}'
   FROM dbo.UH_DEMO d1
   WHERE d1.id = n.id
@@ -139,3 +139,5 @@ SELECT n.id, STUFF(
 FROM dbo.Name n
 WHERE n.Member_Type = 'M'
 ORDER BY n.id
+
+
