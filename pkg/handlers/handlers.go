097a1178aabcf0f7327fdfeba3643c7685e3d88f
Ka Mok <kmok@anthemengineering.com>
2017-09-13 16:21:33 -0400

    Refactor the external elastic package into a service.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,40 +1,16 @@
 package handlers
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
-	"gopkg.in/olivere/elastic.v5"
 	"net/http"
-	"sync"
 
-	"membership-analytics/config"
-)
-
-var (
-	clientInit sync.Once
-	client     *elastic.Client
+	"membership-analytics/pkg/services/elastic"
 )
 
 // The ResponseValues type describes the structure of the all responses.
 type ResponseValues struct {
 	Values []map[string]interface{}
-}
-
-// Client inits a new client on initial call, and returns the initialized client subsequently
-func Client() *elastic.Client {
-	clientInit.Do(func() {
-		c, err := elastic.NewClient(
-			elastic.SetURL("https://elasticsearch.unitehere.org:9200"),
-			elastic.SetBasicAuth(config.Values.ElasticUsername, config.Values.ElasticPassword),
-			elastic.SetSniff(false),
-			elastic.SetHealthcheck(false))
-		if err != nil {
-			panic(err)
-		}
-		client = c
-	})
-	return client
 }
 
 // GetSearchSSN returns a fuzzy matched array of imis_id given a ssn
@@ -46,17 +22,8 @@ func GetSearchSSN(w http.ResponseWriter, r *http.Request) {
 		writeError(w, http.StatusBadRequest, errors.New("You need to pass in a ssn string of at least 7 digits as a q param"))
 		return
 	}
-	ctx := context.Background()
 
-	query := elastic.NewMatchQuery("demographics.ssn", ssnQuery[0]).Fuzziness("Auto")
-
-	searchResult, err := Client().Search().
-		Index(config.Values.Index).
-		Query(query).
-		Pretty(true).
-		FetchSourceContext(elastic.NewFetchSourceContext(true).Include("imis_id", "demographics.ssn")).
-		Do(ctx)
-
+	searchResult, err := elastic.SearchSSN(ssnQuery[0])
 	if err != nil {
 		writeError(w, http.StatusBadRequest, err)
 		return
