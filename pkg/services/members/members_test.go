28663527465481c1306d0493baddd1cbe2088788
Ka Mok <kmok@anthemengineering.com>
2017-09-15 10:30:07 -0400

    Refactor into using table driven testing

--- pkg/services/members/members_test.go
+++ pkg/services/members/members_test.go
@@ -1,6 +1,7 @@
 package members
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,146 +10,38 @@ import (
 	elastic "gopkg.in/olivere/elastic.v5"
 )
 
-func TestSearchSSNNoHits(t *testing.T) {
-	handler := http.NotFound
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		handler(w, r)
-	}))
-	defer ts.Close()
-
-	handler = func(w http.ResponseWriter, r *http.Request) {
-		resp := `{
-      "took": 0,
-      "timed_out": false,
-      "_shards": {
-        "total": 5,
-        "successful": 5,
-        "failed": 0
-      },
-      "hits": {
-        "total": 0,
-        "max_score": null,
-        "hits": []
-      }
-    }`
-
-		w.Write([]byte(resp))
+func TestSearchSSN(t *testing.T) {
+	cases := []struct {
+		testDataPath string
+		want         []map[string]interface{}
+	}{
+		{"testdata/notfound.json", []map[string]interface{}{}},
+		{"testdata/onefound.json", []map[string]interface{}{map[string]interface{}{"imis_id": "5962"}}},
+		{"testdata/multiplefound.json", []map[string]interface{}{map[string]interface{}{"demographics": map[string]interface{}{"ssn": "123456789"}, "imis_id": "5962"}, map[string]interface{}{"imis_id": "5965", "demographics": map[string]interface{}{"ssn": "123456788"}}}},
 	}
 
-	s, err := MockService(ts.URL)
-	assert.NoError(t, err)
+	for _, testCase := range cases {
+		handler := http.NotFound
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			handler(w, r)
+		}))
+		defer ts.Close()
 
-	actualResult, err := s.SearchSSN("123456789")
-	assert.NoError(t, err)
-	expectedResult := []map[string]interface{}{}
-	assert.Equal(t, expectedResult, actualResult)
-}
+		resp, err := ioutil.ReadFile(testCase.testDataPath)
+		assert.NoError(t, err)
 
-func TestSearchSSNOneHit(t *testing.T) {
-	handler := http.NotFound
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		handler(w, r)
-	}))
-	defer ts.Close()
+		handler = func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(resp))
+		}
 
-	handler = func(w http.ResponseWriter, r *http.Request) {
-		resp := `{
-      "took": 1,
-      "timed_out": false,
-      "_shards": {
-        "total": 5,
-        "successful": 5,
-        "failed": 0
-      },
-      "hits": {
-        "total": 1,
-        "max_score": 7.411551,
-        "hits": [
-          {
-            "_index": "members-2017.09.14",
-            "_type": "member",
-            "_id": "AV5_Z3USj5SI64ssYb5t",
-            "_score": 7.411551,
-            "_source": {
-              "imis_id": "5962",
-              "demographics": {
-                "ssn": "123456789"
-              }
-            }
-          }
-        ]
-      }
-    }`
-		w.Write([]byte(resp))
+		s, err := MockService(ts.URL)
+		assert.NoError(t, err)
+
+		actualResult, err := s.SearchSSN("123456789")
+		assert.NoError(t, err)
+
+		assert.Equal(t, testCase.want, actualResult)
 	}
-
-	s, err := MockService(ts.URL)
-	assert.NoError(t, err)
-
-	actualResult, err := s.SearchSSN("123456789")
-	assert.NoError(t, err)
-
-	assert.Equal(t, "5962", actualResult[0]["imis_id"])
-}
-
-func TestSearchSSNMultipleHit(t *testing.T) {
-	handler := http.NotFound
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		handler(w, r)
-	}))
-	defer ts.Close()
-
-	handler = func(w http.ResponseWriter, r *http.Request) {
-		resp := `{
-      "took": 1,
-      "timed_out": false,
-      "_shards": {
-        "total": 5,
-        "successful": 5,
-        "failed": 0
-      },
-      "hits": {
-        "total": 1,
-        "max_score": 7.411551,
-        "hits": [
-          {
-            "_index": "members-2017.09.14",
-            "_type": "member",
-            "_id": "AV5_Z3USj5SI64ssYb5t",
-            "_score": 7.411551,
-            "_source": {
-              "imis_id": "5962",
-              "demographics": {
-                "ssn": "123456789"
-              }
-            }
-          },
-          {
-            "_index": "members-2017.09.14",
-            "_type": "member",
-            "_id": "AV5_Z3USj5SI64ssYb6t",
-            "_score": 7.411551,
-            "_source": {
-              "imis_id": "5965",
-              "demographics": {
-                "ssn": "123456788"
-              }
-            }
-          }
-        ]
-      }
-    }`
-
-		w.Write([]byte(resp))
-	}
-
-	s, err := MockService(ts.URL)
-	assert.NoError(t, err)
-
-	actualResult, err := s.SearchSSN("123456789")
-
-	assert.Equal(t, "5962", actualResult[0]["imis_id"])
-	assert.Equal(t, "5965", actualResult[1]["imis_id"])
 }
 
 func MockService(url string) (Service, error) {
