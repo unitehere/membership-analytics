67f18788a6b237c987c01e712dee0e19150c87df
Ka Mok <kmok@anthemengineering.com>
2017-09-21 20:34:24 -0400

    Add handler tests for PostSearchName

--- pkg/handlers/handlers_test.go
+++ pkg/handlers/handlers_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -54,6 +55,55 @@ func TestGetSearchSSN(t *testing.T) {
 	}
 }
 
+func TestPostSearchName(t *testing.T) {
+	cases := []struct {
+		requestPath        string
+		expectedResultPath string
+		status             int
+	}{
+		{"TestSearchName/invalid_request1.json", "TestSearchName/invalidinput_response.json", 400},
+		{"TestSearchName/invalid_request2.json", "TestSearchName/invalidinput_response.json", 400},
+		{"TestSearchName/onefound_request.json", "TestSearchName/onefound_response.json", 200},
+		{"TestSearchName/notfound_request.json", "TestSearchName/notfound_response.json", 200},
+		// TODO multiple found
+		// {"404040404", "TestSearchName/multiplefound_response.json", 200},
+	}
+	membersService = mockService{}
+
+	for _, testCase := range cases {
+		req, err := http.NewRequest("POST", "search/name", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		requestBytes, err := ioutil.ReadFile(testCase.requestPath) // read request as bytes
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Body = ioutil.NopCloser(strings.NewReader(string(requestBytes))) // convert from bytes to string, then set as body
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(PostSearchName)
+		handler.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		actualStatus := rr.Code
+		assert.Equal(t, testCase.status, actualStatus, "they should be equal")
+
+		// Check the response body is what we expect.
+		expectedResultBytes, err := ioutil.ReadFile(testCase.expectedResultPath)
+		assert.NoError(t, err)
+		var expectedResult map[string]interface{}
+		err = json.Unmarshal(expectedResultBytes, &expectedResult)
+		assert.NoError(t, err)
+
+		var response map[string]interface{}
+		err = json.Unmarshal(rr.Body.Bytes(), &response)
+		assert.NoError(t, err)
+
+		assert.Equal(t, expectedResult, response)
+	}
+}
+
 type mockService struct {
 }
 
@@ -71,5 +121,12 @@ func (s mockService) SearchSSN(ssnQuery members.SSNQuery) (map[string]members.Me
 }
 
 func (s mockService) SearchName(nameQuery members.NameQuery) (map[string]members.Member, error) {
-	return map[string]members.Member{}, nil
+	var result []map[string]interface{}
+	if nameQuery.FirstName == "Alberto" { // handlers test, one found
+		result = []map[string]interface{}{map[string]interface{}{"imis_id": "18775", "first_name": "Alberto", "last_name": "Monteiro"}}
+	} else {
+		result = []map[string]interface{}{}
+	}
+	member := members.Member{Data: result, TotalHits: int64(len(result))}
+	return map[string]members.Member{"members": member}, nil
 }
