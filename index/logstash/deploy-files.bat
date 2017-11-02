97a6a84f951d20741e593246d190eb67b052774b
Han Xiao <hxiao@anthemengineering.com>
2017-11-02 07:19:15 -0400

    Update Deploy Script and Audit Log SQL
    Update audit log SQL statement and deploy script for audit log
    ingest.

--- index/logstash/deploy-files.bat
+++ index/logstash/deploy-files.bat
@@ -1,4 +1,6 @@
 xcopy.exe /s/y %0\..\index-members.conf C:\Elastic\
 xcopy.exe /s/y %0\..\ingest-members.sql C:\Elastic\
+xcopy.exe /s/y %0\..\index-audit-log.conf C:\Elastic\
+xcopy.exe /s/y %0\..\ingest-audit-log.sql C:\Elastic\
 xcopy.exe /s/y/e/i %0\..\sqljdbc_6.2 C:\Elastic\sqljdbc_6.2\
 xcopy.exe /s/y/e/i %0\..\post-logstash-script C:\Elastic\post-logstash-script\
---
+++ index/logstash/ingest-audit-log.sql
@@ -0,0 +1,16 @@
+SELECT(
+  SELECT audit_log_id AS audit_log_id,
+         old_value AS old_value,
+         new_value AS new_value,
+	     log_type AS log_type,
+	     record_id AS imis_id,
+	     record_seqn AS imis_seqn,
+	     entity AS entity,
+	     property AS property,
+	     audit_date AS audit_date,
+		 0 AS ssn_decrypted
+  FOR JSON PATH, WITHOUT_ARRAY_WRAPPER
+)
+AS audit_log
+FROM dbo.UH_AUDIT_LOG
+WHERE AUDIT_DATE >= DATEADD(day, -11117, GETDATE()) AND ENTITY='UH_DEMO' AND PROPERTY='SSN' 
