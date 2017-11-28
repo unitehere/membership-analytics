8e854521052236511396bdc5b7d867a23695d1c1
Han Xiao <hxiao@anthemengineering.com>
2017-11-28 06:17:33 -0500

    Add Pylint
    Add pylint to dev dependencies.

--- flaskapp/config/common.py
+++ flaskapp/config/common.py
@@ -1,4 +1,4 @@
 SECRET_KEY = '?\xbf,\xb4\x8d\xa3"<\x9c\xb0@\x0f5\xab,w\xee\x8d$0\x13\x8b83'
 
 ELASTICSEARCH_HOST = "https://admin:admin@elasticsearch.unitehere.org:9200/"
-ELASTICSEARCH_INDEX = "members-2017.11.02"
+ELASTICSEARCH_INDEX = "members"
